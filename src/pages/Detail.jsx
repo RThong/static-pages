59df75187ca64bd058ee6517da93f085c5199241
rutao.hong@elestyle.jp <rutao.hongelestyle.jp@rutaohongelestylejpnoMacBook-Pro.local>
2024-12-11 16:23:44 +0900

    init

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Detail() {
+  return (
+    <div className="detail-page">
+      <h1>Detail Page11222</h1>
+      <a href="/">Back to Home</a>
+    </div>
+  )
+}
+
+export default Detail 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="home-page">
+      <h1>Home Page</h1>
+      <a href="/detail">Go to Detail122</a>
+    </div>
+  )
+}
+
+export default Home 
